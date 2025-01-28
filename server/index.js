7afc7870947ff39dc47ad92b393eb85b7c8a1c8b
Vettrikanth <msvettrikanth@gmail.com>
2025-01-28 14:01:59 +0530

    removed comment lines

--- server/index.js
+++ server/index.js
@@ -5,20 +5,18 @@ const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
 
-let userCount = 0; // Counter for users
+let userCount = 0; // Count for users
 
 io.on('connection', (socket) => {
-    userCount++; // Increment user count
-    const username = `User${userCount}`; // Assign a unique username
+    userCount++; 
+    const username = `User${userCount}`;
     console.log(`${username} connected`);
 
-    // Listen for messages from this user
     socket.on('message', (message) => {
         console.log(`${username} said: ${message}`);
-        io.emit('message', `${username} said: ${message}`); // Broadcast the message
+        io.emit('message', `${username} said: ${message}`); 
     });
 
-    // Handle user disconnect
     socket.on('disconnect', () => {
         console.log(`${username} disconnected`);
     });
